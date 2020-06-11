d4eefe9781fa5d592562b6767397ff14e62ff976
Joe <17028719+MixV2@users.noreply.github.com>
2020-06-11 17:38:45 +0100

    Update index.js

--- index.js
+++ index.js
@@ -49,6 +49,32 @@ app.use("/party", require(`${__dirname}/routes/services/party`))
 global.launcherversion = fs.readFileSync(`${__dirname}/public/files/version`).toString()
 global.serverversion = "1.5"
 
+//hopefully this should fix the memory leak issue
+setInterval(() => {
+    console.log("checking for dead parties");
+
+    parties.forEach(party => {
+        // check if the member still exists in xmpp, if not delete them and then do the check
+        party.members.forEach(member => {
+            if (!xmppClients[member.account_id]) {
+                let index = party.members.indexOf(member);
+                if (index !== 1) {
+                    party.members.splice(index, 1);
+                    console.log("removed member from party, no longer present in xmpp");
+                }
+            }
+        });
+
+        if (party.members.length <= 0) {
+            let index = parties.indexOf(party);
+            if (index !== -1) {
+                parties.splice(index, 1);
+                console.log("removed party, member count == 0");
+            }
+        }
+    });
+}, 30000);
+
 app.listen(process.env.port || config.port || 80, () => {
     logging.fdev(`Created by Slushia and Cyuubi, Version \x1b[36m${serverversion}`)
     logging.fdev(`Listening on port \x1b[36m${process.env.port || config.port || 80}`)

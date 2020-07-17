704be524dddb1777977a817c6d3d93aac972ed44
Slushia <teamslushia@gmail.com>
2020-07-17 09:47:11 -0400

    validation ip fix

--- model/User.js
+++ model/User.js
@@ -14,10 +14,6 @@ const schema = new mongoose.Schema({
         type: String,
         required: true
     },
-    ip: {
-        type: String,
-        required: true
-    },
     id: {
         type: String,
         default: crypto.randomBytes(16).toString("hex")
